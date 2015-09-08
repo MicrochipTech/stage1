a02a94d3d3d9cf3950f1f2dd9666e15b64c46156
Paul Thacker <paul.thacker@microchip.com>
2015-09-08 15:37:04 -0700

    Initial commit.

---
+++ src/pic32mzda.c
@@ -0,0 +1,160 @@
+/*********************************************************************
+ *
+ *                  PIC32 Boot Loader
+ *
+ *********************************************************************
+ * FileName:        pic32mzda.c
+ * Dependencies:
+ * Processor:       PIC32MZDA
+ *
+ * Compiler:        MPLAB XC32
+ *                  MPLAB X IDE
+ * Company:         Microchip Technology, Inc.
+ *
+ * Software License Agreement
+ *
+ * The software supplied herewith by Microchip Technology Incorporated
+ * (the “Company”) for its PIC32 Microcontroller is intended
+ * and supplied to you, the Company’s customer, for use solely and
+ * exclusively on Microchip PIC32 Microcontroller products.
+ * The software is owned by the Company and/or its supplier, and is
+ * protected under applicable copyright laws. All rights are reserved.
+ * Any use in violation of the foregoing restrictions may subject the
+ * user to criminal sanctions under applicable laws, as well as to
+ * civil liability for the breach of the terms and conditions of this
+ * license.
+ *
+ * THIS SOFTWARE IS PROVIDED IN AN “AS IS” CONDITION. NO WARRANTIES,
+ * WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
+ * TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
+ * IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
+ * CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ *
+ *
+ * $Id:  $
+ * $Name: $
+ *
+ **********************************************************************/
+#include <stdlib.h>
+
+// DEVCFG4
+#pragma config SWDTPS = SPS1048576      // Sleep Mode Watchdog Timer Postscaler (1:1048576)
+
+// DEVCFG3
+#pragma config EXTDDRSIZE = DDR_SIZE_128MB// External DDR2 SDRAM Memory Size (External DDR2 Size is 128 MB)
+#pragma config FMIIEN = OFF             // Ethernet RMII/MII Enable (RMII Enabled)
+#pragma config FETHIO = ON              // Ethernet I/O Pin Select (Default Ethernet I/O)
+#pragma config PGL1WAY = OFF            // Permission Group Lock One Way Configuration (Allow multiple reconfigurations)
+#pragma config PMDL1WAY = OFF           // Peripheral Module Disable Configuration (Allow multiple reconfigurations)
+#pragma config IOL1WAY = OFF            // Peripheral Pin Select Configuration (Allow multiple reconfigurations)
+
+// DEVCFG2
+#pragma config FPLLIDIV = DIV_3         // System PLL Input Divider (3x Divider)
+#pragma config FPLLRNG = RANGE_5_10_MHZ // System PLL Input Range (5-10 MHz Input)
+#pragma config FPLLICLK = PLL_POSC      // System PLL Input Clock Selection (POSC is input to the System PLL)
+#pragma config FPLLMULT = MUL_50        // System PLL Multiplier (PLL Multiply by 50)
+#pragma config FPLLODIV = DIV_2         // System PLL Output Clock Divider (2x Divider)
+#pragma config VBATBOREN = OFF          // VBAT BOR Enable (Disable ZPBOR during VBAT Mode)
+#pragma config DSBOREN = OFF            // Deep Sleep BOR Enable (Disable ZPBOR during Deep Sleep Mode)
+#pragma config DSWDTPS = DSPS32         // Deep Sleep Watchdog Timer Postscaler (1:2^36)
+#pragma config DSWDTOSC = SOSC          // Deep Sleep WDT Reference Clock Selection (Select SOSC as DSWDT Reference Clock)
+#pragma config DSWDTEN = OFF            // Deep Sleep Watchdog Timer Enable (Disable DSWDT during deep sleep)
+#pragma config FDSEN = OFF              // Deep Sleep Enable (Disable DSEN bit in DSCON)
+#pragma config UPLLFSEL = FREQ_24MHZ    // USB PLL Input Frequency Selection (USB PLL input is 24 MHz)
+
+// DEVCFG1
+#pragma config FNOSC = SPLL             // Oscillator Selection Bits (System PLL)
+#pragma config DMTINTV = WIN_127_128    // DMT Count Window Interval (Window/Interval value is 127/128 counter value)
+#pragma config FSOSCEN = OFF            // Secondary Oscillator Enable (Disable Secondary Oscillator)
+#pragma config IESO = OFF               // Internal/External Switch Over (Disabled)
+#pragma config POSCMOD = EC             // Primary Oscillator Configuration (External clock mode)
+#pragma config OSCIOFNC = ON            // CLKO Output Signal Active on the OSCO Pin (Enabled)
+#pragma config FCKSM = CSECMD           // Clock Switching and Monitor Selection (Clock Switch Enabled, FSCM Disabled)
+#pragma config WDTPS = PS1048576        // Watchdog Timer Postscaler (1:1048576)
+#pragma config WDTSPGM = STOP           // Watchdog Timer Stop During Flash Programming (WDT stops during Flash programming)
+#pragma config WINDIS = NORMAL          // Watchdog Timer Window Mode (Watchdog Timer is in non-Window mode)
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Disabled)
+#pragma config FWDTWINSZ = WINSZ_25     // Watchdog Timer Window Size (Window size is 25%)
+#pragma config DMTCNT = DMT31           // Deadman Timer Count Selection (2^31 (2147483648))
+#pragma config FDMTEN = OFF             // Deadman Timer Enable (Deadman Timer is disabled)
+
+// DEVCFG0
+#pragma config DEBUG = OFF              // Background Debugger Enable (Debugger is disabled)
+#pragma config JTAGEN = ON              // JTAG Enable (JTAG Port Enabled)
+#pragma config ICESEL = ICS_PGx1        // ICE/ICD Comm Channel Select (Communicate on PGEC1/PGED1)
+#pragma config TRCEN = OFF              // Trace Enable (Trace features in the CPU are disabled)
+#pragma config BOOTISA = MIPS32         // Boot ISA Selection (Boot code and Exception code is MIPS32)
+#pragma config FECCCON = OFF_UNLOCKED   // Dynamic Flash ECC Configuration (ECC and Dynamic ECC are disabled (ECCCON bits are writable))
+#pragma config FSLEEP = VREGS           // Flash Sleep Mode (Flash power down is controlled by the VREGS bit)
+#pragma config DBGPER = ALLOW_PG2       // Debug Mode CPU Access Permission (Allow CPU access to Permission Group 2 permission regions)
+#pragma config SMCLR = MCLR_NORM        // Soft Master Clear Enable (MCLR pin generates a normal system Reset)
+#pragma config SOSCGAIN = GAIN_1X       // Secondary Oscillator Gain Control bits (1x gain setting)
+#pragma config SOSCBOOST = OFF          // Secondary Oscillator Boost Kick Start Enable bit (Normal start of the oscillator)
+#pragma config POSCGAIN = GAIN_2X       // Primary Oscillator Gain Control bits (2x gain setting)
+#pragma config POSCBOOST = ON           // Primary Oscillator Boost Kick Start Enable bit (Boost the kick start of the oscillator)
+#pragma config EJTAGBEN = NORMAL        // EJTAG Boot Enable (Normal EJTAG functionality)
+
+// DEVCP0
+#pragma config CP = OFF                 // Code Protect (Protection Disabled)
+
+// SEQ0
+
+// DEVSN0
+
+// DEVSN1
+
+void JumpToApp(void);
+
+/********************************************************************
+* Function: 	main()
+*
+* Precondition: 
+*
+* Input: 		None.
+*
+* Output:		None.
+*
+* Side Effects:	None.
+*
+* Overview: 	Main entry function. If there is a trigger or 
+*				if there is no valid application, the device 
+*				stays in firmware upgrade mode.
+*
+*			
+* Note:		 	None.
+********************************************************************/
+int main(void)
+{
+	// No trigger + valid application = run application.
+	JumpToApp();
+	return 0;
+}				
+
+
+/********************************************************************
+* Function: 	JumpToApp()
+*
+* Precondition: 
+*
+* Input: 		None.
+*
+* Output:		
+*
+* Side Effects:	No return from here.
+*
+* Overview: 	Jumps to application.
+*
+*			
+* Note:		 	None.
+********************************************************************/
+void JumpToApp(void)
+{	
+	void (*fptr)(void);
+        fptr = (void (*)(void))0x9D004000;
+	fptr();
+}	
+			
+/*********************End of File************************************/
+
+
