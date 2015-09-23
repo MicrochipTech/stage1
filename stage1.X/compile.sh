46038e013b1f35a18d530aa0cbee2e97020d6726
Joshua Henderson <joshua.henderson@microchip.com>
2015-09-23 13:13:57 -0700

    Add error messages if XC32 of MPLAX is missing.

--- stage1.X/compile.sh
+++ stage1.X/compile.sh
@@ -22,8 +22,25 @@ if [ ! -h /usr/bin/mplab_ide ]; then
     die "MPLABX IDE not found.  Please install."
 fi
 
+READLINK=`which readlink`
+XC32OBJCOPY=`which xc32-objcopy`
+
+if [ -z "$READLINK" ] ||
+    [ -z "$XC32OBJCOPY" ]; then
+    echo
+    echo "ERROR: Missing dependencies:\n"
+    echo "READLINK=$READLINK"
+    echo "XC32OBJCOPY=$XC32OBJCOPY"
+    echo
+    exit 1
+fi
+
 dir=$(dirname $0)
 mplab_path=$(dirname $(readlink -n /usr/bin/mplab_ide))
+
+if [ ! -e "$mplab_path/prjMakefilesGenerator.sh" ]; then
+    die "MPLABX install missing prjMakefilesGenerator.sh."
+fi
 
 cd $dir/
 mkdir -p $dir/../uboot/
--- stage1.X/flash.sh
+++ stage1.X/flash.sh
@@ -14,8 +14,22 @@ if [ ! -h /usr/bin/mplab_ide ]; then
     die "MPLABX IDE not found.  Please install."
 fi
 
+READLINK=`which readlink`
+
+if [ -z "$READLINK" ]; then
+    echo
+    echo "ERROR: Missing dependencies:\n"
+    echo "READLINK=$READLINK"
+    echo
+    exit 1
+fi
+
 dir=$(dirname $0)
 mplab_path=$(dirname $(readlink -n /usr/bin/mplab_ide))
+
+if [ ! -e "$mplab_path/mdb.sh" ]; then
+    die "MPLABX install missing mdb.sh."
+fi
 
 sed "s,^Program.*$,Program \"$dir/dist/pic32mzda/production/stage1.X.production.unified.hex\"," $dir/boot_flash.cfg.in > $dir/boot_flash.cfg
 
