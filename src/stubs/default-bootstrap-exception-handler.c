f8154a6030680b30439c9d28ddad7d5381916437
Paul Thacker <paul.thacker@microchip.com>
2015-09-29 14:16:47 -0700

    stage1: Apply Apache 2.0 license

--- src/stubs/default-bootstrap-exception-handler.c
+++ src/stubs/default-bootstrap-exception-handler.c
@@ -1,37 +1,18 @@
 /*********************************************************************
- *
- *        Default Bootstrap Exception Handler Implementation
- *
- *********************************************************************
- * Filename:        default-bootstrap-exception-handler.c
- *
- * Processor:       PIC32
- *
- * Compiler:        MPLAB C Compiler for PIC32 MCUs
- *                  MPLAB IDE
- * Company:         Microchip Technology Inc.
- *
- * Software License Agreement
- *
- * The software supplied herewith by Microchip Technology Incorporated
- * (the 'Company') for its PIC32/PIC24F Microcontroller is intended
- * and supplied to you, the Company's customer, for use solely and
- * exclusively on Microchip PIC32/PIC24F Microcontroller products.
- * The software is owned by the Company and/or its supplier, and is
- * protected under applicable copyright laws. All rights are reserved.
- * Any use in violation of the foregoing restrictions may subject the
- * user to criminal sanctions under applicable laws, as well as to
- * civil liability for the breach of the terms and conditions of this
- * license.
- *
- * THIS SOFTWARE IS PROVIDED IN AN "AS IS" CONDITION. NO WARRANTIES,
- * WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
- * TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
- * PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
- * IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
- * CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- *
- ********************************************************************/
+  Copyright 2015 Microchip Technology, Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ **********************************************************************/
  
 /* This is a default definition of a bootstrap exception handler function.
  * It does nothing and just goes into an infinite loop. If the user
